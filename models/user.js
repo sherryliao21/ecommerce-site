322f3b7200eabd237a7c6985f6a165688c4a92c2
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-06-30 11:28:44 -0500

    feat: create user model

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'user'
+      }
+    },
+    {}
+  )
+  User.associate = function (models) {
+    User.hasMany(models.Order)
+  }
+  return User
+}
