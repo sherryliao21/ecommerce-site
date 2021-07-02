21f4e28b29d60f93e3d7e94da18d15bef321236e
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-02 22:55:39 +0800

    feat: add 'allowNull: false' to quantity attribute in create cart model migration

--- migrations/20210702053233-create-cart.js
+++ migrations/20210702053233-create-cart.js
@@ -9,7 +9,8 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       quantity: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
