ef6bbad248f0673ac25fef3b5fa7d7d571586833
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-07 23:17:02 +0800

    feat: add passport-facebook authentication strategy

---
+++ routes/modules/auth.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+router.get('/facebook', passport.authenticate('facebook', {
+  scope: ['email', 'public_profile']
+}))
+
+router.get('/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
+
+
+module.exports = router
