392d7a8f6dd65fe0b0791d0769809381e7f82872
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-07-08 07:17:55 -0700

    feat: add pagination on products page

--- services/productService.js
+++ services/productService.js
@@ -6,6 +6,9 @@ const productService = {
   getProducts: async (req, res, callback) => {
     let whereQuery = {}
     let categoryId = ''
+    const page = Number(req.query.page) || 1
+    const PAGE_LIMIT = 16
+    const offset = (page - 1) * PAGE_LIMIT
 
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId)
@@ -14,13 +17,28 @@ const productService = {
 
     const [categories, result] = await Promise.all([
       Category.findAll({ raw: true, nest: true }),
-      Product.findAndCountAll({ where: whereQuery })
+      Product.findAndCountAll({ where: whereQuery, offset, limit: PAGE_LIMIT })
     ])
+
+    // data for pagination
+    const pages = Math.ceil(result.count / PAGE_LIMIT)
+    const totalPage = Array.from({ length: pages }).map((_, index) => index + 1)
+    const prev = page - 1 < 1 ? 1 : page - 1
+    const next = page + 1 > pages ? pages : page + 1
 
     const data = result.rows.map(product => ({ ...product.dataValues }))
 
-    callback({ products: data, categories, categoryId })
+    callback({
+      products: data,
+      categories,
+      categoryId,
+      page,
+      totalPage,
+      prev,
+      next
+    })
   },
+
   getProduct: async (req, res, callback) => {
     const [categories, product] = await Promise.all([
       Category.findAll({ raw: true, nest: true }),
