777a0f62a9c9264c14a255c172a19038deb2fa8b
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-18 19:52:52 +0800

    fix: change error_msg variable

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(passport.session()) // activate passport session
 app.use(flash())
 app.use((req, res, next) => {
 	res.locals.success_msg = req.flash("success_msg")
-	res.locals.warning_msg = req.flash("warning_msg")
+	res.locals.error_msg = req.flash("error_msg")
 	next()
 })
 
