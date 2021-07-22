89206110cf8db7e5dc33cd9f513c63e0913a5fee
sherryliao21 <sherry.c.liao@gmail.com>
2021-07-22 22:50:17 +0800

    refactor: rewrite code in async/await style

--- config/passport.js
+++ config/passport.js
@@ -1,42 +1,42 @@
-const passport = require("passport")
-const LocalStrategy = require("passport-local").Strategy
-const FacebookStrategy = require("passport-facebook").Strategy
-const GoogleStrategy = require("passport-google-oauth").OAuth2Strategy
-const db = require("../models")
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const db = require('../models')
 const User = db.User
 const Order = db.Order
-const CartItem = db.CartItem
-const Payment = db.Payment
-const bcrypt = require("bcryptjs")
+const bcrypt = require('bcryptjs')
 
 // local strategy
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true, // for flash msg
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true // for flash msg
     },
-    (req, username, password, cb) => {
-      User.findOne({ where: { email: req.body.email } })
-        .then(user => {
-          if (!user) {
-            return cb(
-              null,
-              false,
-              req.flash("error_msg", ["This email is not registered yet!"])
-            )
-          }
-          if (!bcrypt.compareSync(password, user.password)) {
-            return cb(
-              null,
-              false,
-              req.flash("error_msg", "Wrong input of email or password!")
-            )
-          }
-          return cb(null, user) // if success, pass on user info
-        })
-        .catch(err => console.log(err))
+    async (req, username, password, cb) => {
+      try {
+        const { email } = req.body
+        const user = await User.findOne({ where: { email } })
+        if (!user) {
+          return cb(
+            null,
+            false,
+            req.flash('error_msg', ['This email is not registered yet!'])
+          )
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return cb(
+            null,
+            false,
+            req.flash('error_msg', 'Wrong input of email or password!')
+          )
+        }
+        return cb(null, user) // if success, pass on user info
+      } catch (error) {
+        console.log(error)
+      }
     }
   )
 )
@@ -48,7 +48,7 @@ passport.use(
       clientID: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
       callbackURL: process.env.FACEBOOK_CALLBACK,
-      profileFields: ["email", "displayName"],
+      profileFields: ['email', 'displayName']
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
@@ -61,7 +61,7 @@ passport.use(
         await User.create({
           name,
           email,
-          password: hash,
+          password: hash
         })
         return done(null, user)
       } catch (error) {
@@ -78,7 +78,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK,
+      callbackURL: process.env.GOOGLE_CALLBACK
     },
     async (accessToken, refreshToken, profile, done) => {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -101,7 +101,7 @@ passport.deserializeUser(async (id, done) => {
 })
 
 // for JWT strategy
-const passportJWT = require("passport-jwt")
+const passportJWT = require('passport-jwt')
 const ExtractJwt = passportJWT.ExtractJwt
 const JwtStrategy = passportJWT.Strategy
 
@@ -109,18 +109,18 @@ let jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = process.env.JWT_SECRET
 
-let strategy = new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
-  const user = await User.findByPk(jwt_payload.id, {
-    include: [Order],
+passport.use(
+  new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
+    const user = await User.findByPk(jwt_payload.id, {
+      include: [Order]
+    })
+
+    if (!user) {
+      console.log('============ cant find user ===========')
+      return next(null, false)
+    }
+    return next(null, user)
   })
-
-  if (!user) {
-    console.log("============ cant find user ===========")
-    return next(null, false)
-  }
-  return next(null, user)
-})
-
-passport.use(strategy)
+)
 
 module.exports = passport
